cc0465aa155ce6b11088f8740c4c36fb863dd537
Jackzzs <jackzzs@outlook.com>
2023-03-30 15:15:02 +0800

    fix: Bluesea 签到逻辑

--- embykeeper/telechecker/bots/bluesea.py
+++ embykeeper/telechecker/bots/bluesea.py
@@ -6,5 +6,6 @@ from .base import BotCheckin
 class BlueseaCheckin(BotCheckin):
     name = "Bluesea"
     bot_username = "blueseamusic_bot"
+    bot_checkin_cmd = ["/cancel", "/checkin"]
     bot_captcha_len = 4
     bot_text_ignore = ["会话已取消", "没有活跃的会话"]
