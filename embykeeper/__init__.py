548f4747b0335d21233908b6676a8cade8ec368c
Jackzzs <jackzzs@outlook.com>
2023-03-30 15:15:40 +0800

    版本更新

--- embykeeper/__init__.py
+++ embykeeper/__init__.py
@@ -1,5 +1,5 @@
 __author__ = {
     "jackzzs": "jackzzs@outlook.com",
 }
-__version__ = "2.0.5"
+__version__ = "2.0.6"
 __url__ = "https://github.com/embykeeper/embykeeper"
--- setup.py
+++ setup.py
@@ -30,6 +30,6 @@ setup(
     name="embykeeper",
     packages=find_packages(include=["embykeeper", "embykeeper.*"]),
     url="https://github.com/embykeeper/embykeeper",
-    version="2.0.5",
+    version="2.0.6",
     zip_safe=False,
 )
