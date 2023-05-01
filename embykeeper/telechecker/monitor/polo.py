ce968383a6e61f1158db9d0b6b5776db5d5c36fd
Jackzzs <jackzzs@outlook.com>
2023-05-02 02:00:07 +0800

    Add polo monitor

---
+++ embykeeper/telechecker/monitor/polo.py
@@ -0,0 +1,19 @@
+import asyncio
+from pyrogram.types import Message
+
+from .base import Monitor
+
+
+class PoloMonitor(Monitor):
+    name = "Polo"
+    chat_name = "poloemby"
+    chat_keyword = r"普通可用的注册码:\n([\s\S]*)"
+    bot_username = "emby_test_bot"
+    notify_create_name = True
+
+    async def on_trigger(self, message: Message, keys, reply):
+        for code in keys[0].split("\n"):
+            await self.client.send_message(self.bot_username, f"/invite {code}")
+            await self.client.send_message(self.bot_username, self.unique_name)
+            await asyncio.sleep(0.5)
+            self.log.bind(notify=True).info(f'已向Bot发送邀请码: "{code}", 请查看.')
