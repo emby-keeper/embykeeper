f5afd944cf8f95f32f7f76e7dcec154b857bf228
Jackzzs <jackzzs@outlook.com>
2023-05-17 04:19:51 +0800

    Fix nebula ban prompt
    Former-commit-id: 3d0ab916069a0d191a9b85f74127e42970a7f703

--- embykeeper/telechecker/bots/nebula.py
+++ embykeeper/telechecker/bots/nebula.py
@@ -68,6 +68,9 @@ class NebulaCheckin(BaseBotCheckin):
             async with session.get(url_checkin, headers={"User-Agent": useragent}) as resp:
                 results = await resp.json()
             message = results["message"]
+            if "未找到用户" in message:
+                self.log.info("签到失败: 账户错误.")
+                return self.fail()
             if "失败" in message:
                 self.log.info("签到失败.")
                 return self.fail()
