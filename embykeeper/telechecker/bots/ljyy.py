493c5ff436ea8aa2005debbf6c01ad7ef36aaa7f
Jackzzs <jackzzs@outlook.com>
2023-06-19 16:08:00 +0800

    Add delicated model for ljyy

--- embykeeper/telechecker/bots/ljyy.py
+++ embykeeper/telechecker/bots/ljyy.py
@@ -1,9 +1,13 @@
 from .base import AnswerBotCheckin
 
+from pyrogram.types import Message
 from pyrogram.errors import BadRequest
+from thefuzz import fuzz
 
 
 class LJYYCheckin(AnswerBotCheckin):
+    ocr = "cchars4@v1"
+    
     name = "垃圾影音"
     bot_username = "zckllflbot"
     bot_captcha_len = 4
@@ -17,3 +21,11 @@ class LJYYCheckin(AnswerBotCheckin):
             except (BadRequest, TimeoutError):
                 pass
         await super().retry()
+        
+    async def on_captcha(self, message: Message, captcha: str):
+        async with self.operable:
+            if not self.message:
+                await self.operable.wait()
+            match = [(k, fuzz.ratio(k, captcha)) for k in self.get_keys(self.message)]
+            max_k, max_r = max(match, key=lambda x: x[1])
+            await self.message.click(max_k)
