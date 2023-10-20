d394f3dbb7273dfc6ab5dc1fd13f58fb43d52310
Jackzzs <jackzzs@outlook.com>
2023-10-20 11:24:31 +0800

    Formatting

--- utils/test_ddddocr.py
+++ utils/test_ddddocr.py
@@ -5,13 +5,15 @@ from embykeeper.utils import AsyncTyper
 
 app = AsyncTyper()
 
+
 @app.async_command()
 async def main(image: Path, beta: bool = False):
     ocr = DdddOcr(beta=beta, show_ad=False)
-    with open(image, 'rb') as f:
+    with open(image, "rb") as f:
         image = f.read()
-    
+
     print(ocr.classification(image))
-    
+
+
 if __name__ == "__main__":
     app()
