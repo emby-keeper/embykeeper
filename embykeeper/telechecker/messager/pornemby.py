94fdd4dfe79455e7f02d787144930dfc53167e94
jackzzs <jackzzs@outlook.com>
2023-10-24 15:33:15 +0800

    Fix: nohp error

--- embykeeper/telechecker/messager/pornemby.py
+++ embykeeper/telechecker/messager/pornemby.py
@@ -9,7 +9,7 @@ class PornembyMessager(Messager):
     default_messages = ["pornemby-common-wl@latest.yaml * 100"]
 
     def prepare_send(self, message):
-        if pornemby_status["nohp"] >= datetime.today().date():
+        if pornemby_status.get("nohp", None) and pornemby_status["nohp"] >= datetime.today().date():
             self.log.warning(f"取消发送: 血量已耗尽.")
         else:
             return message
