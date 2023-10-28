97cd440aba0b3c14c89490f5516052b113995137
jackzzs <jackzzs@outlook.com>
2023-10-29 06:41:32 +0800

    Formatting

--- windows/installer-script/script/cli.py
+++ windows/installer-script/script/cli.py
@@ -4,7 +4,7 @@ from embykeeper.cli import app
 from embykeeper.var import console
 
 if __name__ == "__main__":
-    os.system('cls')
-    console.rule('Embykeeper')
+    os.system("cls")
+    console.rule("Embykeeper")
     console.print()
     app()
