da60f9e668ff1819f721b1ed5fa76524b07dbc84
jackzzs <jackzzs@outlook.com>
2023-10-30 12:46:15 +0800

    Formatting

--- embykeeper/telechecker/bots/embyhub.py
+++ embykeeper/telechecker/bots/embyhub.py
@@ -4,4 +4,4 @@ from .base import BotCheckin
 class EmbyHubCheckin(BotCheckin):
     name = "EmbyHub"
     bot_username = "EdHubot"
-    bot_checked_keywords = ['今日已签到']
+    bot_checked_keywords = ["今日已签到"]
