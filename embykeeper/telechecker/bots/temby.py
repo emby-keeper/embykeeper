f065162437cd6b97174e9f10a2895d6851bfe56f
jackzzs <jackzzs@outlook.com>
2023-10-30 12:45:45 +0800

    Update checkin prompt handling

--- embykeeper/telechecker/bots/temby.py
+++ embykeeper/telechecker/bots/temby.py
@@ -8,7 +8,7 @@ class TembyCheckin(AnswerBotCheckin):
     bot_username = "HiEmbyBot"
     bot_checkin_cmd = "/hi"
     bot_success_keywords = ["Checkin successfully"]
-    bot_fail_keywords = ["Checkin failed"]
+    bot_checked_keywords = ["you have checked in already today"]
 
     async def on_answer(self, message: Message):
         await super().on_answer(message)
