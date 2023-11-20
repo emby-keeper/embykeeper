9c0da838d608f60b583486c8f42de6fdc6f55a31
ZJ Song <23294384+Nigel-NI@users.noreply.github.com>
2023-11-21 00:32:26 +0800

    Update terminus.py (#42)

--- embykeeper/telechecker/bots/terminus.py
+++ embykeeper/telechecker/bots/terminus.py
@@ -8,4 +8,4 @@ class TerminusCheckin(BotCheckin):
     bot_username = "EmbyPublicBot"
     bot_checkin_cmd = ["/cancel", "/checkin"]
     bot_text_ignore = ["会话已取消", "没有活跃的会话"]
-    bot_captcha_len = 2
+    bot_captcha_len = 3
