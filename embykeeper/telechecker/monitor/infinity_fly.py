626b88a402d936ee684f040e004c47e3aca7dd8b
jackzzs <jackzzs@outlook.com>
2024-02-07 05:46:42 +0800

    Add inifinity fly monitor

---
+++ embykeeper/telechecker/monitor/infinity_fly.py
@@ -0,0 +1,16 @@
+from pyrogram.types import Message
+
+from .base import Monitor
+
+class InfinityFlyMonitor(Monitor):
+    name = "Infinity Fly"
+    chat_keyword = r"register-[-\w]{36}"
+    bot_username = "Infinity94bot"
+    chat_name = "infinityfly664"
+    notify_create_name = True
+    allow_edit = False
+
+    async def on_trigger(self, message: Message, key, reply):
+        await self.client.send_message(self.bot_username, f"/invite {key}")
+        await self.client.send_message(self.bot_username, f"/create {self.unique_name}")
+        self.log.bind(notify=True).info(f'已向 Bot @{self.bot_username} 发送了邀请码: "{key}", 请查看.')
