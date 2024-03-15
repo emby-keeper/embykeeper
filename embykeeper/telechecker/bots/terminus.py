aa81b439d18e21c209091949c2e599ed3b5832b0
jackzzs <jackzzs@outlook.com>
2024-03-16 01:19:46 +0800

    Try to fix misty

--- embykeeper/telechecker/bots/terminus.py
+++ embykeeper/telechecker/bots/terminus.py
@@ -36,7 +36,7 @@ class TerminusCheckin(AnswerBotCheckin):
                     self.log.warning(f"远端解析失败, 正在重试解析 ({i + 1}/3).")
             else:
                 self.log.warning(f"签到失败: 验证码识别错误.")
-                await self.fail()
+                return await self.fail()
             result = options[options_cleaned.index(result)]
             try:
                 await message.click(result)
