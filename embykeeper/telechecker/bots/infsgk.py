6587b328567085430033b2070167b02421b6b2b5
Judah Gao <98164810+GrandDuke1106@users.noreply.github.com>
2024-06-20 04:15:03 +0800

    情报局社工库换了机器人 (#61)

--- embykeeper/telechecker/bots/infsgk.py
+++ embykeeper/telechecker/bots/infsgk.py
@@ -5,7 +5,7 @@ __ignore__ = True
 
 class InfSGKCheckin(BotCheckin):
     name = "情报局社工库"
-    bot_username = "InfSGK_bot"
+    bot_username = "infSGK0_bot"
     bot_checkin_cmd = "/sign"
     bot_success_keywords = ["签到成功"]
     bot_checked_keywords = ["您已经签到过了"]
