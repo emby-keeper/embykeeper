919edbb66e8c2d3709f2a960a4141ffa360ea0d0
jackzzs <jackzzs@outlook.com>
2024-10-02 19:56:53 +0800

    Fix marmot

--- embykeeper/telechecker/bots/marmot_group.py
+++ embykeeper/telechecker/bots/marmot_group.py
@@ -5,7 +5,7 @@ from ._base import BotCheckin
 
 class MarmotGroupCheckin(BotCheckin):
     name = "Marmot 群组发言"
-    chat_name = "Marmot_Emby"
+    chat_name = -1001975531465
     bot_checkin_cmd = ["签到", "打劫", "没币了", "低保", "打卡", "冒泡"]
     skip = 14
 
