50ba08d23e81483a3469dc958f52818245cf6cee
jackzzs <jackzzs@outlook.com>
2024-10-02 19:55:03 +0800

    Add template B

---
+++ embykeeper/telechecker/bots/_templ_b.py
@@ -0,0 +1,12 @@
+from ._base import BotCheckin
+
+__ignore__ = True
+
+
+class TemplateBCheckin(BotCheckin):
+    bot_checkin_cmd = "/checkin"
+    bot_use_captcha = False
+    
+
+def use(**kw):
+    return type("TemplatedClass", (TemplateBCheckin,), kw)
