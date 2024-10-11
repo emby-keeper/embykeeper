eeed6a5348e318d7d3bb4709f50ae92c18e634ca
jackzzs <jackzzs@outlook.com>
2024-10-11 19:55:52 +0800

    Add feiyue group test

---
+++ embykeeper/telechecker/bots/test_feiyue_group.py
@@ -0,0 +1,8 @@
+from .feiyuemusic import FeiyueMusicGroupCheckin
+
+__ignore__ = True
+
+
+class TestFeiyueGroupCheckin(FeiyueMusicGroupCheckin):
+    name = "飞跃星空群组签到测试"
+    chat_name = "api_group"
