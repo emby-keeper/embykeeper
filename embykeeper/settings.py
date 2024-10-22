f9410c858d68afd5f7b4413f8e56e677a01f5e55
jackzzs <jackzzs@outlook.com>
2024-10-22 15:38:01 +0800

    Change default random setting, and add help message for wrong config

--- embykeeper/settings.py
+++ embykeeper/settings.py
@@ -143,8 +143,8 @@ def write_faked_config(path, quiet=False):
     doc.add(comment("最大可同时进行的 Telegram Bot 签到."))
     doc["concurrent"] = 1
     doc.add(nl())
-    doc.add(comment("计划任务时, 各站点之间签到的随机时间差异 (分钟)."))
-    doc["random"] = 15
+    doc.add(comment("计划任务时, 各站点之间签到的随机时间差异 (分钟), 所有站点签到将在该时间范围内完成."))
+    doc["random"] = 60
     doc.add(nl())
     doc.add(comment(f"代理设置, Emby 和 Telegram 均将通过此代理连接, 服务器位于国内时请配置代理并取消注释."))
     proxy = item(
@@ -433,8 +433,15 @@ async def prepare_config(config_file=None, basedir=None, public=False, windows=F
                 logger.error(f"TOML 配置文件错误: {e}.")
                 sys.exit(252)
     error = check_config(config)
+    error_str = str(error)
     if error:
-        logger.error(f"配置文件错误, 请检查配置文件:\n{error}.")
+        if "'true' should be instance of 'bool'" in error_str:
+            error_str += '\n说明: 您可能使用了 "true" 或 \'true\' 作为是/否的配置, 请去除引号 (xxx = true).'
+        if "'false' should be instance of 'bool'" in error_str:
+            error_str += '\n说明: 您可能使用了 "false" 或 \'false\' 作为是/否的配置, 请去除引号 (xxx = false).'
+        if "should be instance of 'int'" in error_str:
+            error_str += f'\n说明: 您可能对需要使用数值的参数设置了字符串, 请去除引号 (xxx = 1234).'
+        logger.error(f"配置文件错误, 请检查配置文件 (参考: https://emby-keeper.github.io/guide/配置指南):\n{error_str}")
         sys.exit(253)
     proxy: dict = config.get("proxy", None)
     if proxy:
