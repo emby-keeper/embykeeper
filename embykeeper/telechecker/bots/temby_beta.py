709ff65973f42a632e5a8a292ba3111bb4e65d1c
jackzzs <jackzzs@outlook.com>
2024-10-27 17:05:03 +0800

    Fix temby beta username

--- embykeeper/telechecker/bots/temby_beta.py
+++ embykeeper/telechecker/bots/temby_beta.py
@@ -5,5 +5,5 @@ __ignore__ = True
 
 class TembyBetaCheckin(TembyCheckin):
     name = "Temby"
-    bot_username = "HiEmbyBot"
+    bot_username = "Temby_Bot"
     bot_account_fail_keywords = ["需要内测码"]
